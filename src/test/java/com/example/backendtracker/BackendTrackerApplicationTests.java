ef1d4c5bb8e2e8b9206df5ce6de67a9f9d503323
Danila <67117334+danilakla@users.noreply.github.com>
2024-07-14 11:33:59 +0300

    init project

---
+++ src/test/java/com/example/backendtracker/BackendTrackerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.backendtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@SpringBootTest
+class BackendTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
